d265dec9c3ee288328779d9ccd4f1fd29a21e48b
kangyoungmin <kangym855@gmail.com>
2023-11-13 11:09:59 +0900

    1113_crud 모두 완성!!

--- src/components/Bbs.jsx
+++ src/components/Bbs.jsx
@@ -1,34 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, FormControl, Button, FormGroup } from 'react-bootstrap';
-import { db } from "../firebase";
-import { addDoc, deleteDoc, getDoc, collection, updateDoc, doc } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, FormGroup, FormControl, Button, ListGroup } from 'react-bootstrap';
+import { db } from '../firebase';
+import { addDoc, deleteDoc, getDocs, updateDoc, collection, doc } from 'firebase/firestore';
 
 const Bbs = () => {
-    const [title, setTitle]= useState("");
-    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState('');
     const [id, setId] = useState("");
+    const [val, setVal] = useState([]);
+    const [show, setShow] = useState(false);
 
     const value = collection(db, "demodb");
 
-    const handleCreate = async () =>{    
-        await addDoc(value, { title, content });
+    useEffect(() => {
+        const getData = async () => {
+            const res = await getDocs(value);
+            setVal(res.docs.map(rs => ({ ...rs.data(), id: rs.id })))
+        }
+        getData();
+    })
+
+    const handleCreate = async () => {
+        await addDoc(value, { title, content })
         setTitle('');
         setContent('');
     }
 
+    const handleDelete = async (id) => {
+        if (window.confirm("정말로 삭제하시겠습니까?")) {
+            const delId = doc(db, "demodb", id);
+            await deleteDoc(delId);
+        }
+    }
+
+    const handleUpdate = async () => {
+        const updateId = doc(db, "demodb", id);
+        await updateDoc(updateId, { title, content })
+        setShow(false);
+        setTitle("");
+        setContent("");
+        setId("");
+    }
+    const handleEdit = (id, title, content) => {
+        setTitle(title)
+        setContent(content)
+        setId(id)
+        setShow(true);
+    }
     return (
-        <Container className='mt-5 pt-5'>
-            <Row className='mb-5 pb-3'>
-                <Col md="6">
+        <Container className="mt-5 pt-5">
+            <Row className="mb-5 pb-3">
+                <Col>
                     <FormGroup>
-                        <FormControl type='text' onChange={(e)=>setTitle(e.target.value)} placeholder='제목' className='mb-3' value={title} />
-                        <FormControl as='textarea' onChange={(e)=>setContent(e.target.value)} placeholder='내용' rows={3} className='mb-3' value={content} />
-                        <Button variant='secondary' id="inputG" onClick={handleCreate} className='d-block'>전송</Button>
+                        <FormControl type="text" onChange={(e) => setTitle(e.target.value)} placeholder='제목' className="mb-3" value={title} />
+                        <FormControl as="textarea" onChange={(e) => setContent(e.target.value)} placeholder='내용' rows={3} className="mb-3" value={content} />
+                        {!show ? <Button variant="secondary" id="inputG" onClick={handleCreate} className="d-block">전송</Button> : <Button variant="danger" className="d-block" onClick={handleUpdate}> 수정 </Button> }
                     </FormGroup>
                 </Col>
             </Row>
             <Row>
-                
+                <Col>
+                    <ListGroup as="ol" numbered>
+                        {
+                            val.map((values, index) =>
+                                <ListGroup.Item key={index} className="d-flex justify-content-between align-items-start p-3">
+                                    <Row>
+                                        <Col md="10" className="ps-3 ">
+                                            <h5>{values.title}</h5>
+                                            <p>{values.content}</p>
+                                        </Col>
+                                        <Col md="2" className="btn-box text-end align-self-center">
+                                            <Button variant="danger m-2" onClick={() => handleDelete(values.id)}>삭제</Button>
+                                            <Button variant="warning m-2" onClick={() => handleEdit(values.id, values.title, values.content)}>수정</Button>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )
+                        }
+                    </ListGroup>
+                </Col>
             </Row>
         </Container>
     )
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,19 @@
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
-import {getAuth } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    //firebase에서는 아래와 같이 dotenv를 설정해줘야 한다.
+
+    // apiKey: "AIzaSyDTd-YvQLUdx1yvaVSNIOKDn6_k3qHOvQQ",
+    // authDomain: "ymkangfirebasestart.firebaseapp.com",
+    // projectId: "ymkangfirebasestart",
+    // storageBucket: "ymkangfirebasestart.appspot.com",
+    // messagingSenderId: "742915925814",
+    // appId: "1:742915925814:web:ad2e74b845c58fe0d97597",
+    // measurementId: "G-FJH55NV48L"
+
     apiKey: `${process.env.REACT_APP_KEY}`,
     authDomain: `${process.env.REACT_APP_DOMAIN}`,
     projectId: `${process.env.REACT_APP_PROJECTID}`,
